45be3cbb6a719b4c64ef998c66305af9ea6232dc
Chris Stevenson <chris.j.stevenson@gmail.com>
2016-07-19 09:30:39 -0500

    site design changes

--- server/routes.js
+++ server/routes.js
@@ -3,5 +3,6 @@ var loginController = require('./controllers/loginController');
 
 module.exports = function (app) {
     app.get('/', viewController.index);
+    app.get('/register', viewController.register);
     app.post('/api/login', loginController.login);
 }
